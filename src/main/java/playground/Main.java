eda5359341e4c49c29f098379865c7e35a15f88b
Joel Courtney <joel.e.courtney@gmail.com>
2021-03-21 17:43:00 -0600

    comments on main

--- src/main/java/playground/Main.java
+++ src/main/java/playground/Main.java
@@ -2,18 +2,29 @@ package playground;
 
 import parspice.ParSPICE;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import parspice.sender.DoubleArraySender;
 import spice.basic.CSPICE;
 
 public class Main {
     private static final int iterations = 10000000;
     private static ParSPICE par;
 
+    /*
+    Runs the same task three times:
+
+    1. 6 workers, just outputs in ParSPICE
+    2. 6 workers, both inputs and outputs in ParSPICE
+    3. Single-threaded through direct JNISpice
+
+    The task is to call vhat `iterations` times with the input `new double[]{1, 2, i}`
+    where i increments up from 0 to `iterations`.
+
+    The timing results are printed to stdout.
+     */
     public static void main(String[] args) throws Exception {
+        System.out.println("\nRunning with " + iterations + " iterations\n");
         par = new ParSPICE("build/libs/worker-1.0-SNAPSHOT.jar", 50050);
 
         long start = System.currentTimeMillis();
@@ -25,18 +36,14 @@ public class Main {
         System.out.println("Input + Output ParSPICE: " + (System.currentTimeMillis() - start));
 
         start = System.currentTimeMillis();
-        System.loadLibrary("JNISpice");
-        List<double[]> r3 = new ArrayList<>(iterations);
-        for (int i = 0; i < iterations; i++) {
-            r3.add(CSPICE.vhat(new double[]{1, 2, i}));
-        }
+        List<double[]> r3 = direct();
         System.out.println("Direct JNISpice: " + (System.currentTimeMillis() - start));
 
 
         for (int i = 0; i < iterations; i++) {
             for (int j = 0; j < 3; j++) {
                 if (r1.get(i)[j] != r2.get(i)[j] || r1.get(i)[j] != r3.get(i)[j]) {
-                    System.out.println("r1: " + Arrays.toString(r1.get(i)));
+                    System.out.println("\nr1: " + Arrays.toString(r1.get(i)));
                     System.out.println("r2: " + Arrays.toString(r2.get(i)));
                     System.out.println("r3: " + Arrays.toString(r3.get(i)));
                 }
@@ -44,6 +51,9 @@ public class Main {
         }
     }
 
+    /*
+    Run the task with no inputs phase.
+     */
     public static List<double[]> withInput() throws Exception {
         List<double[]> inputs = new ArrayList<>(iterations);
         for (int i = 0; i < iterations; i++) {
@@ -56,11 +66,26 @@ public class Main {
         );
     }
 
+    /*
+    Run the task with inputs and outputs phase.
+     */
     public static List<double[]> noInput() throws Exception {
         return par.run(
                 new MyWorker(),
                 iterations,
                 6
         );
+    }
+
+    /*
+    Run the task directly through JNISpice.
+     */
+    public static List<double[]> direct() throws Exception {
+        System.loadLibrary("JNISpice");
+        List<double[]> results = new ArrayList<>(iterations);
+        for (int i = 0; i < iterations; i++) {
+            results.add(CSPICE.vhat(new double[]{1, 2, i}));
+        }
+        return results;
     }
 }
