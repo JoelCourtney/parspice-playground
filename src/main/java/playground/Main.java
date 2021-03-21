634aa1997131176fab17be06c823a2c5ba5b160f
Joel Courtney <joel.e.courtney@gmail.com>
2021-03-21 13:58:25 -0600

    test sending inputs

--- src/main/java/playground/Main.java
+++ src/main/java/playground/Main.java
@@ -1,18 +1,65 @@
 package playground;
 
 import parspice.ParSPICE;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import parspice.sender.DoubleArraySender;
+import spice.basic.CSPICE;
 
 public class Main {
+    private static final int iterations = 10000000;
+
     public static void main(String[] args) throws Exception {
-        int iterations = 10000000;
         long start = System.currentTimeMillis();
-        List<double[]> results = ParSPICE.run(
+        List<double[]> r1 = noInput();
+        System.out.println("NoInput ParSPICE: " + (System.currentTimeMillis() - start));
+
+        start = System.currentTimeMillis();
+        List<double[]> r2 = withInput();
+        System.out.println("Input ParSPICE: " + (System.currentTimeMillis() - start));
+
+        start = System.currentTimeMillis();
+        System.loadLibrary("JNISpice");
+        List<double[]> r3 = new ArrayList<>(iterations);
+        for (int i = 0; i < iterations; i++) {
+            r3.add(CSPICE.vhat(new double[]{1, 2, i}));
+        }
+        System.out.println("Direct JNISpice: " + (System.currentTimeMillis() - start));
+
+
+        for (int i = 0; i < iterations; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (r1.get(i)[j] != r2.get(i)[j] || r1.get(i)[j] != r3.get(i)[j]) {
+                    System.out.println(Arrays.toString(r1.get(i)));
+                    System.out.println(Arrays.toString(r2.get(i)));
+                    System.out.println(Arrays.toString(r3.get(i)));
+                }
+            }
+        }
+    }
+
+    public static List<double[]> withInput() throws Exception {
+        List<double[]> inputs = new ArrayList<>(iterations);
+        for (int i = 0; i < iterations; i++) {
+            inputs.add(new double[]{1, 2, i});
+        }
+        ParSPICE<double[], double[]> par2 = new ParSPICE<>(
+                "build/libs/worker-1.0-SNAPSHOT.jar",
+                "playground.MySecondWorker",
+                new DoubleArraySender(3),
+                new DoubleArraySender(3)
+        );
+        return par2.run(inputs, 6);
+    }
+
+    public static List<double[]> noInput() throws Exception {
+        ParSPICE<Void, double[]> par = new ParSPICE<>(
                 "build/libs/worker-1.0-SNAPSHOT.jar",
                 "playground.MyWorker",
-                new DoubleArraySender(3), iterations, 6
+                new DoubleArraySender(3)
         );
-        System.out.println("ParSPICE: " + (System.currentTimeMillis() - start));
+        return par.run(iterations, 6);
     }
 }
--- src/main/java/playground/MySecondWorker.java
+++ src/main/java/playground/MySecondWorker.java
@@ -1,13 +1,14 @@
 package playground;
 
 import parspice.sender.DoubleArraySender;
+import parspice.worker.InputWorker;
 import spice.basic.CSPICE;
 import spice.basic.SpiceErrorException;
-import parspice.worker.NoInputWorker;
 
-public class MyWorker extends NoInputWorker<double[]> {
+public class MySecondWorker extends InputWorker<double[], double[]> {
     public static void main(String[] args) throws Exception {
-        new MyWorker().run(
+        new MySecondWorker().run(
+                new DoubleArraySender(3),
                 new DoubleArraySender(3),
                 args
         );
@@ -19,7 +20,7 @@ public class MyWorker extends NoInputWorker<double[]> {
     }
 
     @Override
-    public double[] task(int i) throws SpiceErrorException {
-        return CSPICE.vhat(new double[]{1, 2, i});
+    public double[] task(double[] in) throws SpiceErrorException {
+        return CSPICE.vhat(in);
     }
 }
